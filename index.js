bb5af357c40f2ac3226d4c3acb942a5f9b0763de
Navaro <26208752+navar0wned@users.noreply.github.com>
2024-10-29 23:22:24 +0100

    Moved index.html and some fixes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on  http://localhost:${PORT}`);
